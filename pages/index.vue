9886fc0ef671054d4073d707bce71dd9553f4045
the7of9 <72879120+the7of9@users.noreply.github.com>
2024-04-29 12:12:14 +0200

    feat: homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-import { components } from '~/slices'
+import { components } from "~/slices";
 
-const prismic = usePrismic()
-const { data: page } = useAsyncData('index', () =>
-  prismic.client.getByUID('page', 'home')
-)
+const prismic = usePrismic();
+const { data: page } = useAsyncData("index", () =>
+  prismic.client.getByUID("page", "home")
+);
 
-useHead({
-  title: prismic.asText(page.value?.data.title)
-})
+useSeoMeta({
+  title: page.value?.data.meta_title ?? undefined,
+  description: page.value?.data.meta_description ?? undefined,
+  ogImage: prismic.asImageSrc(page.value?.data.meta_image) ?? undefined,
+});
 </script>
-
 
 <template>
   <SliceZone
