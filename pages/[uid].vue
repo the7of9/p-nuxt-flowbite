b31b22c53d9c5f02c63037d95fef904d583b5fac
the7of9 <72879120+the7of9@users.noreply.github.com>
2024-04-30 12:29:41 +0200

    feat: multiple pages

--- pages/[uid].vue
+++ pages/[uid].vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
-import { components } from '~/slices'
+import { components } from "~/slices";
 
-const prismic = usePrismic()
-const route = useRoute()
+const prismic = usePrismic();
+const route = useRoute();
 const { data: page } = useAsyncData(route.params.uid as string, () =>
-  prismic.client.getByUID('page', route.params.uid as string)
-)
+  prismic.client.getByUID("page", route.params.uid as string, {
+    fetchLinks: [
+      "testimonial.quote",
+      "testimonial.avatar",
+      "testimonial.name",
+      "testimonial.job_title",
+    ],
+  })
+);
 
-useHead({
-  title: prismic.asText(page.value?.data.title)
-})
+useSeoMeta({
+  title: page.value?.data.meta_title,
+  description: page.value?.data.meta_description,
+});
 </script>
-
 
 <template>
   <SliceZone
